d70f015e2787cc89da18bd657cde2aae33387174
david-rivera-s <da.rivera@movilis.edu.ec>
2026-07-16 16:01:28 -0500

    fix: corregir renderizado de la tabla de películas y estructurar datos

--- src/components/TablaPeliculas.jsx
+++ src/components/TablaPeliculas.jsx
@@ -1,4 +1,4 @@
-import "./TablaPeliculas.css"
+// import "./TablaPeliculas.css"
 
 export const TablaPeliculas = ({ peliculas }) => {
     return(
@@ -6,20 +6,22 @@ export const TablaPeliculas = ({ peliculas }) => {
             <table>
                 <thead>
                     <tr>
-                    <th>Título</th>
-                    <th>Género</th>
-                    <th>Views</th>
-                    <th>Director</th>
+                        <th>Título</th>
+                        <th>Género</th>
+                        <th>Views</th>
+                        <th>Director</th>
                     </tr>
                 </thead>
                 <tbody>
                     {peliculas.map((p) => {
-                        <tr>
-                            <td>{p.titulo}</td>
-                            <td>{p.genero}</td>
-                            <td>{p.views}</td>
-                            <td>{p.director}</td>
-                        </tr>
+                        return(
+                            <tr key={p.id}>
+                                <td>{p.titulo}</td>
+                                <td>{p.genero}</td>
+                                <td>{p.views}</td>
+                                <td>{p.director}</td>
+                            </tr>
+                        )
                     })}
                 </tbody>
             </table>
--- src/data/peliculas.js
+++ src/data/peliculas.js
@@ -1,29 +1,34 @@
 export const peliculas = [
     {
+        id: 1,
         titulo: "Inception",
         genero: "Ciencia Ficción",
         views: 150000000,
         director: "Christopher Nolan"
     },
     {
+        id: 2,
         titulo: "El Padrino",
         genero: "Drama / Crimen",
         views: 95000000,
         director: "Francis Ford Coppola"
     },
     {
+        id: 3,
         titulo: "El Viaje de Chihiro",
         genero: "Animación / Fantasía",
         views: 85000000,
         director: "Hayao Miyazaki"
     },
     {
+        id: 4,
         titulo: "Pulp Fiction",
         genero: "Crimen / Comedia Negra",
         views: 110000000,
         director: "Quentin Tarantino"
     },
     {
+        id: 5,
         titulo: "Parásitos",
         genero: "Suspenso / Drama",
         views: 75000000,

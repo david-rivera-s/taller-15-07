ddba11feabf6f6e7a12fb1bfec8272b79c1babd2
david-rivera-s <da.rivera@movilis.edu.ec>
2026-07-16 15:40:29 -0500

    feat: agregar carpetas de componentes y datos, y actualizar App.jsx

---
+++ src/components/TablaPeliculas.jsx
@@ -0,0 +1,28 @@
+import "./TablaPeliculas.css"
+
+export const TablaPeliculas = ({ peliculas }) => {
+    return(
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                    <th>Título</th>
+                    <th>Género</th>
+                    <th>Views</th>
+                    <th>Director</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {peliculas.map((p) => {
+                        <tr>
+                            <td>{p.titulo}</td>
+                            <td>{p.genero}</td>
+                            <td>{p.views}</td>
+                            <td>{p.director}</td>
+                        </tr>
+                    })}
+                </tbody>
+            </table>
+        </div>
+    )
+}
---
+++ src/data/peliculas.js
@@ -0,0 +1,32 @@
+export const peliculas = [
+    {
+        titulo: "Inception",
+        genero: "Ciencia Ficción",
+        views: 150000000,
+        director: "Christopher Nolan"
+    },
+    {
+        titulo: "El Padrino",
+        genero: "Drama / Crimen",
+        views: 95000000,
+        director: "Francis Ford Coppola"
+    },
+    {
+        titulo: "El Viaje de Chihiro",
+        genero: "Animación / Fantasía",
+        views: 85000000,
+        director: "Hayao Miyazaki"
+    },
+    {
+        titulo: "Pulp Fiction",
+        genero: "Crimen / Comedia Negra",
+        views: 110000000,
+        director: "Quentin Tarantino"
+    },
+    {
+        titulo: "Parásitos",
+        genero: "Suspenso / Drama",
+        views: 75000000,
+        director: "Bong Joon Ho"
+    }
+];
